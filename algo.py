6bd3de88c0efebcbee4ffb44a462e133b81a8b4a
“david <davzy007@gmail.com>
2023-11-28 18:15:41 +0000

    completed user input and type validation

--- algo.py
+++ algo.py
@@ -1,3 +1,68 @@
 # STORES ALL THE DATA STRUCTURE ALGORITHMS TO BE USED IN THE PROJECT
 
+# region TYPE VALIDATION
+def is_integer(user_input):
+    try:
+        user_input = int(user_input)
+    except ValueError:
+        return False
 
+    return user_input
+# endregion
+
+# region SORTING ALGORITHMS
+def sort_array(array, start, end):
+    # uses QUICK SORT algorithm to sort an array
+    if end > start:
+        pivot = hoare_partition(array, start, end)
+        sort_array(array, start, pivot - 1)
+        sort_array(array, pivot + 1, end)
+
+    return array
+
+
+def hoare_partition(array, start, end):
+    i = start
+    j = end
+    pivot = array[(start + end) // 2]
+
+    while j > i:
+
+        while array[i] < pivot:
+            i += 1
+
+        while array[j] > pivot:
+            j -= 1
+
+        if i < j:
+            array[i], array[j] = array[j], array[i]
+        else:
+            return j
+
+# endregion
+
+# region SEARCHING ALGORITHMS
+
+
+def binary_search(the_list, search_term):
+    # returns -1 when no match
+    found = False
+    found_term_index = -1
+    start_index = 0
+    end_index = len(the_list) - 1
+
+    while not found and end_index >= start_index:
+        midpoint = (start_index + end_index) // 2
+        if the_list[midpoint] == search_term:
+            found = True
+            found_term_index = midpoint
+
+        elif search_term < the_list[midpoint]:
+            end_index = midpoint - 1
+
+        else:
+            start_index = midpoint + 1
+
+    return found_term_index
+
+# endregion
--- places.py
+++ places.py
@@ -1,26 +1,41 @@
 # THIS MODULE HANDLES PLACES LISTED ON THE APP
+from algo import sort_array
+from algo import binary_search
+from algo import is_integer
 
 
 class Places:
     def __init__(self):
         # list of valid accommodation types
-        self.ACCOMMODATION_TYPES = ["Hotel", "Hostel", "Bed and breakfast", "Apartment", "Guest house", "Dormitory", "Campsite", "Motel", "Cottage", "Resort", "Villa", "Inn", "Chalet", "Lodge", "Homestay", "Log cabin", "Glamping"]
+        self.ACCOMMODATION_TYPES = sort_array(["Hotel", "Hostel", "Bed and breakfast", "Apartment", "Guest house", "Dormitory", "Campsite", "Motel", "Cottage", "Resort", "Villa", "Inn", "Chalet", "Lodge", "Homestay", "Log cabin", "Glamping"], 0, 16)
 
     def valid_accommodation_type(self, user_input):
         # CHECKS IF THE ACCOMMODATION TYPE THE USERS HAS ENTERED CORRESPONDS WITH ANY VALID ACCOM. TYPE
-        return False
+
+        # type validation
+        user_input = is_integer(user_input)
+        if user_input is False:
+            return False
+
+        # index range validation
+        if user_input < 1 or user_input > len(self.ACCOMMODATION_TYPES):
+            return False
+
+        # assign accommodation type
+        chosen_type = self.ACCOMMODATION_TYPES[user_input - 1]
+        return binary_search(self.ACCOMMODATION_TYPES, chosen_type) != -1
 
     def add_new_place(self):
-        # prompts user to enter the name, type, and address of a new place they want to add
+        # prompts user to enter the name, accommodation type, address, available rooms, and cost per night of stay for a new place they want to add
 
         # initial prompt to prepare the user
-        input("We will need a few details about the place\nlike name, accommodation type, address and cost per night of stay.\nPress enter to continue")
+        input("We will need a few details about the place\nlike name, accommodation type, address, available rooms, and cost per night of stay.\nPress enter to continue")
         print("\n")
 
         # to get a valid name longer than 1 char
-        np_name = input("Please enter the name of this new place\n")
+        np_name = ""
         while len(np_name) < 2:
-            np_name = input("Please enter a valid name for this new place!\n")
+            np_name = input("Please enter a valid name for this new place:\n")
 
         # to print out a list of valid accommodation types and prompt the user to select
         type_index = 0
@@ -28,8 +43,18 @@ class Places:
             type_index += 1
             print(f"{type_index}. {t}")
         print(f"What type of place would {np_name} be?")
-        np_type = input("*Select accommodation type from menu (enter name or number)*: ")
+        np_type = ""
+        while not self.valid_accommodation_type(np_type):
+            np_type = input("*Select a valid accommodation type from list (enter number)*: ")
 
         np_address = input(f"Where would {np_name} be located?:\n")
+        while len(np_address) < 2:
+            np_address = input(f"Please enter a valid address for {np_name}:\n")
+
+        np_available_rooms = input(f"How many rooms will be available initially?:\n")
+        while not is_integer(np_available_rooms):
+            np_available_rooms = input(f"Please enter a number greater than 0 for initial rooms available at {np_name}:\n")
 
         np_price_per_night = input(f"Lastly, how much would {np_name} cost per night:\n")
+        while not is_integer(np_price_per_night):
+            np_price_per_night = input(f"Please enter a number greater than 0 for {np_name}'s price per night\n")
