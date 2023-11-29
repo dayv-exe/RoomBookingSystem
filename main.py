8a50b66cdc6964b303bdcb45c67af7bbf13c5a7b
“david <davzy007@gmail.com>
2023-11-29 12:54:29 +0000

    project structure related bug fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import algo
+from algo import storage
 from places import Places
 place = Places()
 
 # CREATE CSV FILES WITH HEADERS FOR STORAGE WHEN PROGRAM IS FIRST RUN (IF FILES DON'T EXIST)
-algo.initialize_file(place.PLACES_PATH, f"name, type, address, available_rooms, price")
+storage.initialize_file(place.PLACES_PATH, f"name, type, address, available_rooms, price")
 
 place.add_new_place()
